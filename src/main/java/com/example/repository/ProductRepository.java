ef2db4cdec2a93f134ce744e12de4fb7bcb000bb
Yerko Navarro <yerkonavarro7@gmail.com>
2025-11-14 16:20:07 -0300

    elimina redundancias

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.example.repository;
 
 import org.springframework.stereotype.Repository;
 import com.example.backend__pasteleria.entity.ProductEntity;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
-    ProductEntity findByProductEntity(int id);
-    
+    ProductEntity findProductEntityById(int id);
+   
 }
